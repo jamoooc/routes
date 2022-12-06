de1cc32be1c0a1c72815fff17463e90bd971d470
Jamie MacRae <jamoooc@gmail.com>
2022-12-06 00:45:59 +0000

    feat: add route container

---
+++ src/components/Routes/Routes.tsx
@@ -0,0 +1,9 @@
+import classes from "./routes.module.css";
+
+export default function Routes() {
+  return (
+    <div className={classes.routeContainer}>
+      <div className={classes.routeContainer}>hello</div>
+    </div>
+  );
+}
