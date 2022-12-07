df0f1ee6f6686d48e4150fb8ff0449b6b057bb38
Jamie MacRae <jamoooc@gmail.com>
2022-12-07 15:18:43 +0000

    feat: add types file

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type RouteNameType = {
+  commonName: string;
+  naptanID: string;
+};
+
+export type RouteListItemType = {
+  id: number;
+  origin: RouteNameType;
+  destination: RouteNameType;
+};
+
+export type RouteDepartureTimesType = {
+  departureTimes: string[]; // timestamp
+};
