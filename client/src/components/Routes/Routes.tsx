455e92b730395c114672813e238e32af11b3d5a9
Jamie MacRae <jamoooc@gmail.com>
2023-01-11 00:23:50 +0000

    feat: add title to edit route menu

--- client/src/components/Routes/Routes.tsx
+++ client/src/components/Routes/Routes.tsx
@@ -429,6 +429,7 @@ function RouteMenu({
         onFocus={onFocusHandler}
         aria-haspopup="menu"
         aria-expanded={open}
+        title="Delete/Edit"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
